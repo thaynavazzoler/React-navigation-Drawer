622db315a10806157440230a1f40255b5b0494a3
thaynavazzoler <thayna.vazzoler@aluno.senai.br>
2024-03-06 10:40:24 -0300

    "Create pastas"

---
+++ src/screens/Contact/index.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import styles from "./styles";
+
+export default function Contact() {
+  return (
+    <View style={styles.container}>
+      <Text>Contact</Text>
+    </View>
+  );
+}
